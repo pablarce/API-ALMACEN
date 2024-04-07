82901647d8f58f3e23a6ae5b6e3d83cf1a2e84c3
pablarce <113457050+pablarce@users.noreply.github.com>
2024-04-07 15:25:28 +0200

    encriptation for user

--- modules/user/user.model.js
+++ modules/user/user.model.js
@@ -1,36 +1,58 @@
-(function () {
-  var mongoose = require("mongoose");
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var bcrypt = require("bcrypt");
 
-  var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["client", "administrator"],
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  organization: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  country: String,
+});
 
-  var UserSchema = new Schema({
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    role: {
-      type: String,
-      required: true,
-      enum: ["client", "administrator"],
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    organization: {
-      type: String,
-      required: true,
-    },
-    country: String,
+// Antes de guardar el usuario, hashea la contraseña si es nueva o ha sido modificada
+UserSchema.pre("save", function (next) {
+  var user = this;
+  if (!user.isModified("password")) return next();
+
+  // Genera un salt para hashear la contraseña
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    // Hashea la contraseña utilizando el salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+
+      // Reemplaza la contraseña sin hashear con la contraseña hasheada
+      user.password = hash;
+      next();
+    });
   });
+});
 
-  module.exports = mongoose.model("users", UserSchema);
-})();
+module.exports = mongoose.model("User", UserSchema);
