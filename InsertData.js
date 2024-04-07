346c7f6385ffcdb6fe0556b5aba1c6ea9122566b
paulaqing <[paula.diestro@alumnos.uneatlantico.es]>
2024-04-07 18:06:24 +0200

    Create InsertData

---
+++ InsertData.js
@@ -0,0 +1,62 @@
+// insertDistributor.js 
+
+const mongoose = require("mongoose");
+const Distributor = require("./modules/distributor/distributor.model");
+const distributorData = require("./data/distributorData");
+const Equipment = require("./modules/equipment/equipment.model");
+const equipmentData = require("./data/equipmentData");
+const Warehouse = require("./modules/warehouse/warehouse.model");
+const warehouseData = require("./data/warehouseData");
+
+// Conectar a la base de datos MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/API-ALMACEN", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Función para verificar si la base de datos está vacía
+async function isDatabaseEmpty() {
+  const count = await Client.countDocuments();
+  return count === 0;
+}
+
+// Método para insertar los datos en la base de datos
+async function insertData() {
+    try {
+      // Insertar los datos de los distribuidores en la base de datos
+      await Distributor.insertMany(distributorData);
+      console.log("Distributor data inserted into the database successfully!");
+  
+      // Insertar los datos de los almacenes en la base de datos
+      await Warehouse.insertMany(warehouseData);
+      console.log("Warehouse data inserted into the database successfully!");
+  
+      // Insertar los datos de los equipos en la base de datos
+      await Equipment.insertMany(equipmentData);
+      console.log("Equipment data inserted into the database successfully!");
+    } catch (error) {
+      console.error("Error inserting data into the database:", error);
+    } finally {
+      // Desconectar de la base de datos al finalizar
+      mongoose.disconnect();
+    }
+  }
+  
+
+// Método para insertar los datos al inicio si la base de datos está vacía
+async function insertDataAtStart() {
+  try {
+    const isEmpty = await isDatabaseEmpty();
+    if (isEmpty) {
+      console.log("Database is empty, inserting data...");
+      await insertData();
+    } else {
+      console.log("Database is not empty, skipping data insertion.");
+    }
+  } catch (error) {
+    console.error("Error checking database status:", error);
+  }
+}
+
+// Exportar el método insertDataAtStart
+module.exports = insertDataAtStart;
