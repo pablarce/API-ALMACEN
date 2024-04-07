fbcfc6a01a5ad018a6fca554893f705bc37d7210
paulaqing <[paula.diestro@alumnos.uneatlantico.es]>
2024-04-07 21:17:11 +0200

    Create data

---
+++ data/warehouseData.js
@@ -0,0 +1,40 @@
+const warehouseData=[
+    {
+        "name": "Almacén CLE",
+        "location": "Salamanca",
+        "warehouseId": "19283",
+        "capacity": 1400
+    },
+    {
+        "name": "Almacén CMA",
+        "location": "Madrid",
+        "warehouseId": "14320",
+        "capacity": 1500
+    },
+    {
+        "name": "Almacén AND",
+        "location": "Sevilla",
+        "warehouseId": "89001",
+        "capacity": 1200
+    },
+    {
+        "name": "Almacén CAT",
+        "location": "Barcelona",
+        "warehouseId": "45902",
+        "capacity": 1700
+    },
+    {
+        "name": "Almacén CA",
+        "location": "Santander",
+        "warehouseId": "22400",
+        "capacity": 800
+    },
+    {
+        "name": "Almacén CVA",
+        "location": "Valencia",
+        "warehouseId": "93741",
+        "capacity": 1000
+    }
+ ];
+ module.exports = warehouseData;
+ 
